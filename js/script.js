6d983e3e08aecb39e0ebf92a23b6a23392f018e3
Alastair Odhiambo <alastairodhiambo@outlook.com>
2020-09-24 00:25:00 -0400

    updated files

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ gsap.registerPlugin(ScrollTrigger);
 
 const tl= gsap.timeline();
 tl.from(".logo", 
-{duration: 1.5, opacity: 0, scale: 0.3, ease: "back", });
+{delay: 1.5, duration: 1, opacity: 0, y: -300, ease: "back", });
 tl.from("#banner-text", {duration: 2, opacity: 0, y: "random(-200, 200)", stagger: 0.25}, "-=0.5");
 
 //Arrow:
-
+gsap.from(".arrow",{delay: 1.5, opacity: 0, duration: 2})
 gsap.to(".arrow", {y: 12, ease: "power1.inOut", repeat: -1, yoyo: true});
 
 
@@ -18,7 +18,8 @@ gsap.to(".arrow", {y: 12, ease: "power1.inOut", repeat: -1, yoyo: true});
 function goToSection(i, anim) {
   gsap.to(window, {
     scrollTo: {y: i*innerHeight, autoKill: false},
-    duration: 1.0
+    duration: 1.0, 
+    ease: "power1.inOut"
   });
   
   if(anim) {
